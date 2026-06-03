51d3f5d001a50f3fa476762dd3fdefc6ad2c070a
ocrm <107884346+jpeglp@users.noreply.github.com>
2026-06-03 12:19:17 -0700

    Revert "Update tweet-date.tsx"
    This reverts commit bf60277f2eb9e5037e87f3b9dc82086d84752a99.

--- src/components/tweet/tweet-date.tsx
+++ src/components/tweet/tweet-date.tsx
@@ -25,7 +25,7 @@ export function TweetDate({
               viewTweet && 'text-light-secondary dark:text-dark-secondary'
             )}
           >
-            {formatDate(createdAt, 'full')}
+            {formatDate(createdAt, viewTweet ? 'full' : 'tweet')}
           </a>
         </Link>
         <ToolTip
